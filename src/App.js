32a387ce862054ca731b9f483ade47239cf1402a
Mina <bellella13@gmail.com>
2025-03-26 10:48:18 -0400

    success

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1 className="title">We are doing a COMP 300 live demo</h1>
-      <p className="subtitle">GitHub Actions is a powerful tool</p>
+      <h1 className="title">We are doing a COMP 311 live demo</h1>
+      <p className="subtitle">GitHub Actions is a awsome tool</p>
     </div>
   );
 }
